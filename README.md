b5da805848a9fd52dec50f3f8171b18e0fb1d054
Brennan Ashton <bashton@brennanashton.com>
2020-10-10 22:05:38 -0700

    Add the initial boilerplate with GitHub OAuth support
    Signed-off-by: Brennan Ashton <bashton@brennanashton.com>

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# nuttx-testnom
+# NuttX TestNom
 I eat Apache NuttX test reports... nom nom nom
+
+This is a web application for collecting test data for NuttX commits and releases in a more structured way so that it is easier to track the state of things going forward.
+
+Feed me tests at https://nom.bashton.io
+
+## Features Implemented
+ * :(
+
+## Features Planned
+ * Allow users to submit test reports on a given release
+ * Collect build artifacts to make it super easy for users to test without having to rebuild everything all the time.  This would be driven by the Apache NuttX CI system.
+ * Collect metrics on the build using things like Bloaty so that we can generate amazing reports on build size etc...
+
+# Developement
+You will want to create an environment specific configuration file
+```
+GH_CLIENT_ID='SOMETHING'
+GH_CLIENT_SECRET='SOMETHING ELSE'
+SECRET_KEY='shhhh dont tell'
+```
+
+```
+poetry install
+export FLASK_ENV=development
+export FLASK_APP=nxtestnom
+export NOM_CONFIG=/path/to/my/config.py
+poetry run flask run -p 8000
+```
+
+Seeding with data tbd.
+
+
+## Testing
+
+```
+poetry install
+poetry run pytest
+```
---
+++ nxtestnom/__init__.py
@@ -0,0 +1,38 @@
+import sentry_sdk
+
+from flask import Flask, redirect, url_for, request
+from sentry_sdk.integrations.flask import FlaskIntegration
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_dance.contrib.github import make_github_blueprint, github
+
+__version__ = '0.0.1'
+
+app = Flask(__name__)
+app.config.from_envvar('NOM_SETTINGS', silent=True)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+blueprint = make_github_blueprint(
+    client_id=app.config.get('GH_CLIENT_ID', 'fake'),
+    client_secret=app.config.get('GH_CLIENT_SECRET', 'fake'),
+)
+
+app.register_blueprint(blueprint, url_prefix="/login")
+
+if app.config.get('SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=app.config.get('SENTRY_DSN'),
+        integrations=[FlaskIntegration()],
+        _experiments={"auto_enabling_integrations": True},
+    )
+
+@app.route("/")
+def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
